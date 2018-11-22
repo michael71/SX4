ba410f476fc3f57a0913ec6b20f717add2af7a67
Michael Blank <mblank@bawue.de>
2018-11-22 08:16:34 +0100

    updated start script

--- startSX4.sh
+++ startSX4.sh
@@ -1,5 +1,8 @@
 # FCC an USB0
-java -jar dist/SX4.jar -D /dev/ttyUSB0 -t FCC 
+java -jar dist/SX4.jar -s /dev/ttyUSB0 -t FCC -d 
 
 # SLX825 an USB0 
-# java -jar dist/SX4.jar -D /dev/ttyUSB0 -t SLX825 -b 9600
+# java -jar dist/SX4.jar -s /dev/ttyUSB0 -t SLX825 -b 9600 -d
+
+# Simulation only, debugging on
+java -jar dist/SX4.jar -t SIM -d
