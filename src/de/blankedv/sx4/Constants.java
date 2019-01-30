f18023a0ae0a41ae279d2c6a31a84bf60395de58
mblank <mblank@bawue.de>
2019-01-30 15:01:58 +0100

    rev 1.16

--- src/de/blankedv/sx4/Constants.java
+++ src/de/blankedv/sx4/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     /**
      * {@value #VERSION} = program version, displayed in HELP window
      */
-    public static final String VERSION = "SX4 - rev1.15 - 28 Jan 2019";
+    public static final String VERSION = "SX4 - rev1.16 - 30 Jan 2019";
     
     // switch one more debugging?
     // DEBUG can be set via args - and hence is variable
